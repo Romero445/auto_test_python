9831f047ed96bb2cc5b5b26942262d3102da5f59
Romero445 <53151611+Romero445@users.noreply.github.com>
2020-03-11 14:42:31 +0300

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -16,23 +16,23 @@ class LoginGameMag(unittest.TestCase):
         self.driver.get(baseUrl)
         self.driver.implicitly_wait(20)
 
-    def test_login(self):
+    def testLogin(self):
         self.logoButton = self.driver.find_element(By.XPATH,"//button[@class = 'gm-btn gm-btn--login'] //span[@class='gm-btn__login-img gm-btn__login-img--passive gm-btn__logout__img']")
         self.logoButton.click()
-        self.log.info("Вы нажали кнопку")
+        self.log.info("Push button)
         
         self.clearLoggin = self.driver.find_element(By.ID, "login-form-login").clear()
-        self.log.info("Поле чистое")
+        self.log.info("Clear is done")
 
         self.sendLoggin = self.driver.find_element(By.ID, "login-form-login").send_keys("send_login")
-        self.log.info("Вы ввели login")
+        self.log.info("Login is done")
 
         self.sendPassword = self.driver.find_element(By.NAME, "login-form[password]").send_keys("Password")
-        self.log.info("Вы ввели password")
+        self.log.info("Password is done")
 
         self.loginButton = self.driver.find_element(By.XPATH,
                                                     "//div[@class='login-active login-active--show']//button[text()='Войти']").click()
-        self.log.info("Упс ошибка")
+        self.log.info("Error")
 
         time.sleep(5)
 
